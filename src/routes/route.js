0f6a74fd87bc5f26b4f25b54495a8cbbfc7c0636
rjyoti123 <rjyoti270@gmail.com>
2022-11-04 23:03:23 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
+
+const validateToken=require("../middleware/auth")
+
 const userController= require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+
+    res.send("Welcome to my channel!")
+
 })
 
 router.post("/users", userController.createUser  )
 
 router.post("/login", userController.loginUser)
 
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId", validateToken.validateToken, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId",validateToken.validateToken, userController.updateUser)
+
+router.delete("/users/:userId",validateToken.validateToken,userController.deletedSchoolData)
 
 module.exports = router;
+
